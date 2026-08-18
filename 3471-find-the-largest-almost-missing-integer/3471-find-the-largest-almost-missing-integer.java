34a429b0289bb7d5a00b55fead056c0d7d222a48
Aradhaya Rai <aradhayarai2004@gmail.com>
2026-08-18 20:22:46 +0530

    Time: 2 ms (72.63%), Space: 45 MB (57.19%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        
+        int n=nums.length;
+        HashMap<Integer,Integer> hp=new HashMap<>();
+        for(int i=0;i<n;i++){
+            hp.put(nums[i],hp.getOrDefault(nums[i],0)+1);
+        }
+
+        if(k==n){
+            int maxValue=Integer.MIN_VALUE;
+
+            for(int i=0;i<n;i++){
+                maxValue = Math.max(maxValue,nums[i]);
+            }
+            return maxValue;
+        }
+
+        if(k==1){
+            int maxValue=-1;
+            for(int i=0;i<n;i++){
+                if(hp.get(nums[i])==1 && nums[i]>maxValue){
+                    maxValue = nums[i];
+                }
+            }
+            return maxValue;
+        }
+
+         n = n - 1;
+
+        if (nums[0] == nums[n]) {
+            return -1;
+        }
+
+        if (hp.get(nums[0]) == 1 && hp.get(nums[n]) == 1) {
+            return Math.max(nums[0], nums[n]);
+        }
+
+        if (hp.get(nums[0]) == 1 && hp.get(nums[n]) > 1) {
+            return nums[0];
+        }
+
+        if (hp.get(nums[n]) == 1 && hp.get(nums[0]) > 1) {
+            return nums[n];
+        }
+
+        return -1;
+
+    }
+}
