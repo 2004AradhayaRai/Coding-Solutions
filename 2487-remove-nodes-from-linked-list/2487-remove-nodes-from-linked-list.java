73c80de3922f453709781d57936de75d584996b5
Aradhaya Rai <aradhayarai2004@gmail.com>
2026-08-24 22:41:32 +0530

    Time: 17 ms (49.03%), Space: 135.4 MB (22.7%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -14,6 +14,7 @@ class Solution {
         if(head==null || head.next==null){
             return head;
         }
+        
         head.next=removeNodes(head.next);
         if(head.val<head.next.val){
             return head.next;
