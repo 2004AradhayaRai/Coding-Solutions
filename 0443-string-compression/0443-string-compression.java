dadc73eebba407f267744163f8c3bf763e396e26
Aradhaya Rai <aradhayarai2004@gmail.com>
2026-09-12 23:04:48 +0530

    Time: 1 ms (99.92%), Space: 45.8 MB (11.67%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,32 +1,30 @@
 class Solution {
     public int compress(char[] chars) {
-        int n=chars.length;
-
-        int idx=0;
+        
         int i=0;
-        while(i<n){
-            char curr= chars[i];
-            int cnt=0;
+        int idx=0;
 
-            //find cnt of duplicate 
-            while(i<n && chars[i]==curr){
+        int n=chars.length;
+        while(i<n) {
+            char ch=chars[i];
+            int cnt=0;
+            
+            while(i<n && chars[i]==ch) {
                 cnt++;
                 i++;
-            }
 
-            // do the assign work
-            chars[idx] = curr;
+            }
+            chars[idx] = ch;
             idx++;
 
-            if(cnt>1){
-                String cnt_str = String.valueOf(cnt);
-                for(char ch:cnt_str.toCharArray()){
-                    chars[idx]=ch;
+            if(cnt > 1) {
+                String cntStr = String.valueOf(cnt) ; 
+                for (char c : cntStr.toCharArray()) {
+                    chars[idx] = c;
                     idx++;
                 }
             }
         }
         return idx;
-
     }
 }
